018035ad13d0810eef0a193b0e3948d59981ab3a
paek1127 <paek1127@gmail.com>
2024-11-20 16:39:10 +0900

    WiseSaying class 추가

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -12,9 +12,11 @@ public class Main {
 class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
+
         Scanner scanner = new Scanner(System.in);
 
-        int i = 0;
+        int lastId = 0;
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
@@ -23,13 +25,19 @@ class App {
                 break;
             } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String detail = scanner.nextLine();
+                String content = scanner.nextLine();
 
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
 
-                ++i;
-                System.out.println(i + "번 명언이 등록되었습니다.");
+                int id = ++lastId;
+
+                WiseSaying wiseSaying = new WiseSaying();
+                wiseSaying.id = id;
+                wiseSaying.content = content;
+                wiseSaying.author = author;
+
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
             } else if (cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
@@ -37,5 +45,10 @@ class App {
         }
         scanner.close();
     }
+}
 
+class WiseSaying {
+    int id;
+    String content;
+    String author;
 }
